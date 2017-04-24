64ae6f6e336f4a60800c43a9a965e82418640088
ia87 <ia87@ukr.net>
2017-04-24 23:42:34 +0300

    First commit

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task17.task1721;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +12,34 @@ public class Solution {
     public static List<String> allLines = new ArrayList<String>();
     public static List<String> forRemoveLines = new ArrayList<String>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader file1 = new BufferedReader(new FileReader(reader.readLine()));
+            BufferedReader file2 = new BufferedReader(new FileReader(reader.readLine()));
+
+            while (file1.ready()) allLines.add(file1.readLine());
+            while (file2.ready()) forRemoveLines.add(file2.readLine());
+
+        Solution solution = new Solution();
+        try {
+            solution.joinData();
+        } catch (CorruptedDataException e) {
+            e.printStackTrace();
+        }
+
     }
 
+
+
     public void joinData () throws CorruptedDataException {
+
+            if (allLines.containsAll(forRemoveLines)) {
+                allLines.removeAll(forRemoveLines);
+            } else {
+                allLines.clear();
+                throw new CorruptedDataException();
+            }
 
     }
 }
