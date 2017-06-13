357a1a04eca53736444db667f5cd3c5889b74da1
ia87 <ia87@ukr.net>
2017-06-13 14:48:35 +0300

    2001

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2001/Asset.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task20.task2001;
+
+import java.io.*;
+
+public class Asset {
+    public Asset(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Asset asset = (Asset) o;
+
+        if (Double.compare(asset.price, price) != 0) return false;
+        return name != null ? name.equals(asset.name) : asset.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public void save(OutputStream outputStream) throws Exception
+    {
+        PrintWriter printWriter = new PrintWriter(outputStream);
+        printWriter.println(name);
+        printWriter.println(price);
+        printWriter.flush();
+//        printWriter.close();
+    }
+
+    public void load(InputStream inputStream) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        name = reader.readLine();
+        price = Integer.parseInt(reader.readLine());
+//        reader.close();
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task20/task2001/Solution.java
@@ -0,0 +1,113 @@
+package com.javarush.task.task20.task2001;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Читаем и пишем в файл: Human
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+
+            File your_file_name = File.createTempFile("your_file_name", null);
+            OutputStream outputStream = new FileOutputStream(your_file_name);
+            InputStream inputStream = new FileInputStream(your_file_name);
+
+            Human ivanov = new Human("Ivanov", new Asset("home"), new Asset("car"));
+            ivanov.save(outputStream);
+            outputStream.flush();
+
+            outputStream.close();
+
+            Human somePerson = new Human();
+            somePerson.load(inputStream);
+            //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
+            System.out.println(somePerson.equals(ivanov));
+
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+
+    public static class Human {
+        public String name;
+        public List<Asset> assets = new ArrayList<>();
+
+        public Human() {
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Human human = (Human) o;
+
+            if (name != null ? !name.equals(human.name) : human.name != null) return false;
+            return assets != null ? assets.equals(human.assets) : human.assets == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (assets != null ? assets.hashCode() : 0);
+            return result;
+        }
+
+        public Human(String name, Asset... assets) {
+            this.name = name;
+            if (assets != null) {
+                this.assets.addAll(Arrays.asList(assets));
+            }
+        }
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            PrintWriter writer = new PrintWriter(outputStream);
+
+            if (name != null) writer.print(name);
+
+            String isNamePresent = name != null ? "yes" : "no";
+            writer.print(isNamePresent);
+
+            if (name!=null || !name.isEmpty()) writer.print(name);
+
+            if (!assets.isEmpty()) {
+                writer.print(assets.size());
+                for (Asset as : assets) {
+                    as.save(outputStream);
+                }
+            } else writer.print(0);
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            int assetsSize;
+            Asset asset;
+
+            String isNamePresent = reader.readLine();
+            if ("yes".equals(isNamePresent)) name = reader.readLine();
+
+            assetsSize = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < assetsSize; i++) {
+                asset = new Asset("some asset " + i);
+                asset.load(inputStream);
+                assets.add(asset);
+            }
+        }
+    }
+}
+
