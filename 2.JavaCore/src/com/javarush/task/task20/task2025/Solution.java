b1648dbd644780f14012d3e5279b6965703feb44
ia87 <ia87@ukr.net>
2017-06-20 23:40:09 +0300

    2025

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,22 +1,42 @@
 package com.javarush.task.task20.task2025;
 
-/* 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
 Алгоритмы-числа
 */
 public class Solution {
     public static long[] getNumbers(long N) {
-        long[] result = null;
-        String number = N + "";
-        char[] digits = number.toCharArray();
-        int[] d = new int[digits.length];
-        for (int i = 0; i < digits.length; i++) {
-            d[i] = Integer.parseInt(digits[i] + "");
+//        long[] result = null;
+        ArrayList<Long> list = new ArrayList();
+        for (long l = 0; l < N; l++) {
+            int[] digits = getDigits(l);
+            int exponent = digits.length;
+            int sum=0;
+            for (int d : digits) {
+                sum+=l*Math.pow(d, exponent);
+            }
+            if (l == sum) list.add(l);
         }
-
+        Long[] result = list.toArray(new Long[list.size()]);
         return result;
     }
 
-    int[] getDigits()
+    public static int[] getDigits(long N){
+        String number = N + "";
+        char[] chars = number.toCharArray();
+        int[] digits = new int[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            digits[i] = Integer.parseInt(chars[i] + "");
+        }
+        return digits;
+    }
+
+//    public static long getSum()
+
+//    public static int[] getDigits()
 
     public static void main(String[] args) {
 
