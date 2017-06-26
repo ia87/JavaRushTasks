16e50f4880931c3d5247771000d49e30d72643a5
ia87 <ia87@ukr.net>
2017-06-26 16:51:23 +0300

    2102-2106

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2102/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task21.task2102;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+/* 
+Сравниваем модификаторы
+*/
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        int modifiersOfThisClass = Solution.class.getModifiers();
+        System.out.println(isAllModifiersContainSpecificModifier(modifiersOfThisClass, Modifier.PUBLIC));   //true
+        System.out.println(isAllModifiersContainSpecificModifier(modifiersOfThisClass, Modifier.STATIC));   //false
+
+        int modifiersOfMethod = getMainMethod().getModifiers();
+        System.out.println(isAllModifiersContainSpecificModifier(modifiersOfMethod, Modifier.STATIC));      //true
+    }
+
+    public static boolean isAllModifiersContainSpecificModifier (int allModifiers, int specificModifier) throws Exception{
+        Method[] methods = Modifier.class.getMethods();
+        for (Method m : methods) {
+            if (m.getName().startsWith("is") && ((boolean) m.invoke(Method.class, allModifiers)) && ((boolean) m.invoke(Method.class, specificModifier))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static Method getMainMethod() {
+        Method[] methods = Solution.class.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().equalsIgnoreCase("main")) return method;
+        }
+
+        return null;
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task21.task2105;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Исправить ошибку. Сравнение объектов
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31*(first!=null ? first.hashCode() : 0) + (last!=null ? last.hashCode() : 0) ;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Solution)) return false;
+        Solution n = (Solution) o;
+        if (first!=null ? !first.equals(n.first) : n.first!=null) return false;
+        if (last!=null ? !last.equals(n.last) : n.last!=null) return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}
